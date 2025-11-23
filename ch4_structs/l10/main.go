248ae00603c9b468f46bab4e9b8dfcd5d97c2648
Andre <andreemelo@gmail.com>
2025-11-23 21:46:36 +0000

    ch4_structs

---
+++ ch4_structs/l10/main.go
@@ -0,0 +1,31 @@
+package main
+
+type Membership struct {
+	Type             string
+	MessageCharLimit int
+}
+
+type User struct {
+	Name string
+	Membership
+}
+
+func newUser(name string, membershipType string) User {
+	if membershipType == "premium" {
+		return User{
+			Name: name,
+			Membership: Membership{
+				Type:             membershipType,
+				MessageCharLimit: 1000,
+			},
+		}
+	} else {
+		return User{
+			Name: name,
+			Membership: Membership{
+				Type:             membershipType,
+				MessageCharLimit: 100,
+			},
+		}
+	}
+}
